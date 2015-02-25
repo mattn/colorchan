ad02335622930d26a938299dd351f9e1d196f2b6
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-02-25 21:22:26 +0900

    Take command

--- main.go
+++ main.go
@@ -4,8 +4,24 @@ import (
 	"github.com/mattn/go-colorable"
 	"io"
 	"os"
+	"os/exec"
+	"syscall"
 )
 
 func main() {
-	io.Copy(colorable.NewColorableStdout(), os.Stdin)
+	if len(os.Args) == 0 {
+		io.Copy(colorable.NewColorableStdout(), os.Stdin)
+	} else {
+		cmd := exec.Command(os.Args[1], os.Args[2:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = colorable.NewColorableStdout()
+		cmd.Stderr = colorable.NewColorableStderr()
+		err := cmd.Run()
+		if err != nil {
+			if cmd.ProcessState != nil {
+				os.Exit(cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus())
+			}
+			os.Exit(1)
+		}
+	}
 }
